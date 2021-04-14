3bcb42d1ff107e0f3ec01198cece1913aab43519
Elijah Lucian <elijahlucian@gmail.com>
2021-04-14 16:19:37 -0600

    0.3.0 - SketchyBrowser 🎉🎉🎉🎉

--- src/lib/ReactSketchy.tsx
+++ src/lib/ReactSketchy.tsx
@@ -42,7 +42,7 @@ export const ReactSketchy = ({
     };
   }, [sketch, ref, elRef, dimensions, animate, timeOffset]);
 
-  if (elRef?.current) return null;
+  if (elRef) return null;
 
   return <div className={className} ref={ref}></div>;
 };
--- src/lib/SketchBrowser.tsx
+++ src/lib/SketchBrowser.tsx
@@ -1,18 +1,53 @@
 import { Sketch } from "@dank-inc/sketchy";
-import { useState } from "react";
+import React, { cloneElement, useEffect, useState } from "react";
 
 type Props = {
   sketches: Sketch[];
+  children: React.ReactElement;
+  controls?: boolean;
+  dimensions?: [number, number];
+  animate?: boolean;
 };
 
-export const SketchBrowser = ({ sketches }: Props) => {
-  const [sketch, setSketch] = useState("");
+export const SketchBrowser = ({
+  controls,
+  sketches,
+  children,
+  dimensions = [400, 400],
+  animate,
+}: Props) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (!controls) return;
+
+    const controlListener = (e: KeyboardEvent) => {
+      if (e.key === "e") next();
+      if (e.key === "q") prev();
+    };
+    document.addEventListener("keypress", controlListener);
+
+    return () => document.removeEventListener("keypress", controlListener);
+  });
+
+  const prev = () => {
+    if (index > 0) setIndex(index - 1);
+  };
+  const next = () => {
+    if (index < sketches.length - 1) setIndex(index + 1);
+  };
 
   return (
-    <div className="sketch-nav">
-      <button>Back</button>
-      <p>Sketch: {}</p>
-      <button>Next</button>
-    </div>
+    <>
+      <div className="sketch-nav">
+        <button onClick={prev}>Prev</button>
+        <h1>Sketches!</h1>
+        <p>
+          index: {index + 1} / {sketches.length}
+        </p>
+        <button onClick={next}>Next</button>
+      </div>
+      {cloneElement(children, { sketch: sketches[index], dimensions, animate })}
+    </>
   );
 };
